75ebba7a4ab5b1c89a6fe9b6e5e5437d5587b841
Feyisogo <feyisogoadeyemo.com>
2022-12-14 14:38:50 -0800

    7-print_last_digit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,9 +1,9 @@
-#include "main.h" 
+#include "main.h"
 
 /**  
  * _abs - Computes the absolute value of an integer.
  *  @c: The number to be computed.
- *  Return: Absolute value of number or zero  
+ *  Return: Absolute value of number or zero
  */
 
 int _abs(int c)
