0de451a97fc3c8e9119c19c5c9a1ae09d7f1ed76
Feyisogo <feyisogoadeyemo.com>
2022-12-16 19:51:54 -0800

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,12 +3,12 @@
 /**                                                                                                                                     
  * _isupper - Check if a letter is upper                                                                                                
  * @x: The number to be checked                                                                                                         
- * Return: 1 for upper letter or 0for any else                                                                                           */
+ * Return: 1 for character that will be a digit or 0 for any else 
+ */
 
 int _isupper(int x)                                                                                                                     
 
 {
-	
 	if (x >= 48 && x <= 57)                                                                                                        
 	{
 	return (1);                                                                                                                     
